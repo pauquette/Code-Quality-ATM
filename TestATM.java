93c9e6318a0505447dc30f72c21b15d590f3673a
halbright19 <98185521+halbright19@users.noreply.github.com>
2022-07-13 08:38:52 -0400

    Full code coverage
    Full code coverage in ATM.java, CurrencyExchanger.java, and UnsupportedCurrencyCodeException.java. The only code that is not covered is in the try-catch statements in Test.java

--- TestATM.java
+++ TestATM.java
@@ -39,29 +39,29 @@ class TestATM {
 		atm.withdraw(5);
 		Assert.assertEquals(5, atm.checkBalance(), 0.01);
 	}
-	
+
 	@Test
-	void testCheckCurrency() throws Exception{
+	void testCheckCurrency() throws Exception {
 		ATM atm = new ATM(100, "CAD");
 		Assert.assertEquals("CAD", atm.checkCurrency());
 	}
-	
+
 	@Test
-	void testCheckBalance() throws Exception{
+	void testCheckBalance() throws Exception {
 		ATM atm = new ATM(100, "CAD");
 		Assert.assertEquals(100, atm.checkBalance(), 0.01);
 	}
-	
+
 	@Test
-	void testUSDToCAD() throws Exception{
+	void testUSDToCAD() throws Exception {
 		ATM atm = new ATM(100, "USD");
 		atm.exchangeCurrency("CAD");
 		Assert.assertEquals(130, atm.checkBalance(), 0.01);
 		Assert.assertEquals("CAD", atm.checkCurrency());
 	}
-	
+
 	@Test
-	void testUSDToEUR() throws Exception{
+	void testUSDToEUR() throws Exception {
 		ATM atm = new ATM(100, "USD");
 		atm.exchangeCurrency("EUR");
 		Assert.assertEquals(98, atm.checkBalance(), 0.01);
@@ -69,14 +69,93 @@ class TestATM {
 	}
 
 	@Test
-	void testCurrencyException() throws Exception{	
-		boolean thrown = false;
-		try{
-			@SuppressWarnings("unused")
-			ATM atm = new ATM(100, "USA");
-		}catch(Exception e) {
-			thrown = true;
+	void testUSDError() throws Exception {
+		try {
+			ATM atm = new ATM(100, "USD");
+			atm.exchangeCurrency("");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(),
+					" is not a supported currency to convert to. Supported currencies are: USD, CAD and EUR");
 		}
-	    Assert.assertTrue(thrown);
+	}
+
+	@Test
+	void testCADtoCAD() throws Exception {
+		ATM atm = new ATM(100, "CAD");
+		atm.exchangeCurrency("CAD");
+		Assert.assertEquals(100, atm.checkBalance(), 0.01);
+		Assert.assertEquals("CAD", atm.checkCurrency());
+	}
+
+	@Test
+	void testCADtoUSD() throws Exception {
+		ATM atm = new ATM(100, "CAD");
+		atm.exchangeCurrency("USD");
+		Assert.assertEquals(70, atm.checkBalance(), 0.01);
+		Assert.assertEquals("USD", atm.checkCurrency());
+	}
+
+	@Test
+	void testCADtoEUR() throws Exception {
+		ATM atm = new ATM(100, "CAD");
+		atm.exchangeCurrency("EUR");
+		Assert.assertEquals(76, atm.checkBalance(), 0.01);
+		Assert.assertEquals("EUR", atm.checkCurrency());
+	}
+
+	@Test
+	void testCADError() throws Exception {
+		try {
+			ATM atm = new ATM(100, "CAD");
+			atm.exchangeCurrency("");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(),
+					" is not a supported currency to convert to. Supported currencies are: USD, CAD and EUR");
+		}
+	}
+
+	@Test
+	void testEURtoEUR() throws Exception {
+		ATM atm = new ATM(100, "EUR");
+		atm.exchangeCurrency("EUR");
+		Assert.assertEquals(100, atm.checkBalance(), 0.01);
+		Assert.assertEquals("EUR", atm.checkCurrency());
+	}
+
+	@Test
+	void testEURtoUSD() throws Exception {
+		ATM atm = new ATM(100, "EUR");
+		atm.exchangeCurrency("USD");
+		Assert.assertEquals(102, atm.checkBalance(), 0.01);
+		Assert.assertEquals("USD", atm.checkCurrency());
+	}
+
+	@Test
+	void testEURtoCAD() throws Exception {
+		ATM atm = new ATM(100, "EUR");
+		atm.exchangeCurrency("CAD");
+		Assert.assertEquals(134, atm.checkBalance(), 0.01);
+		Assert.assertEquals("CAD", atm.checkCurrency());
+	}
+
+	@Test
+	void testEURError() throws Exception {
+		try {
+			ATM atm = new ATM(100, "EUR");
+			atm.exchangeCurrency("");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(),
+					" is not a supported currency to convert to. Supported currencies are: USD, CAD and EUR");
+		}
+	}
+
+	@Test
+	void testCurrencyException() throws Exception {
+		try {
+			ATM atm = new ATM(100, "USA");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(),
+					"USA is not a supported currency. Supported currencies are [USD, CAD, EUR]");
+		}
 	}
 }
